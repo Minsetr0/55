9dc8dead5aa5f23036e20dd1030101dd7d3c876e
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 20:39:37 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿int allPictures = 52;
+int picturesInRow = 3;
+int filledRows = 52 / 3;
+int remainingPictures = allPictures % picturesInRow;
+Console.WriteLine("Заполненных рядов:" + filledRows);
+Console.WriteLine("Картинки сверх меры" + remainingPictures);
